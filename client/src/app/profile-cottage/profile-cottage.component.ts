7c1ea5d4edf54dcee6c134ca607046fb842be744
Anđela Paunović <andjelapaunovic99@gmail.com>
2022-01-09 15:28:28 +0100

    Update cottage info if there is no reservations

--- client/src/app/profile-cottage/profile-cottage.component.ts
+++ client/src/app/profile-cottage/profile-cottage.component.ts
@@ -1,9 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { AmenityJSON } from '../dto/amenitiyJSON';
+import { Room } from '../dto/Room';
 import { RuleJSON } from '../dto/RuleJSON';
 import { Endpoint } from '../util/endpoints-enum';
 import { Global } from '../util/global';
@@ -14,6 +16,17 @@ import { Global } from '../util/global';
   styleUrls: ['./profile-cottage.component.css']
 })
 export class ProfileCottageComponent implements OnInit {
+  name !: String
+  address !: String
+  city !: String
+  description !: String
+  maxNumPers !: Number
+  oneDayPrice !: Number
+
+  roomName !: String
+  roomBadTypes !: String
+
+  editButtonClicked !: boolean
 
   imgCollection: Array<object> = [];
 
@@ -22,6 +35,12 @@ export class ProfileCottageComponent implements OnInit {
 
   amenities : AmenityJSON[] = []
   rules : RuleJSON[] = []
+  
+  services = Global.services
+  amenities1 = Global.amenities
+
+  selectedFiles?: FileList;
+  previews: string[] = [];
 
   constructor(private router: Router,private sanitizer: DomSanitizer, private http: HttpClient) { 
     
@@ -99,17 +118,152 @@ export class ProfileCottageComponent implements OnInit {
 
       return ruleJSON
   }
-  removeCottage(){
-    
-  }
-  futureReservations(){
 
+  yourfunctionName(event: any){
+    alert(event)
   }
-  pastReservations(){
-    
-  }
+
   editCottage(){
-    
+    this.editButtonClicked = true
   }
-}
+
+  onSubmit(){
+    this.uploadFiles()
+    
+      this.editButtonClicked = false
+  
+      //update cottage
+      const headers = { 'content-type': 'application/json',
+                        'Authorization': 'Bearer ' + sessionStorage.getItem("token")}  
+      let options = { headers: headers };
+  
+      if(this.name != undefined)
+        this.cottage.name = this.name
+      if(this.address != undefined)
+        this.cottage.address = this.address
+      if(this.city != undefined)
+        this.cottage.city = this.city
+      if(this.oneDayPrice != undefined)
+        this.cottage.oneDayPrice = this.oneDayPrice
+      if(this.description != undefined)
+        this.cottage.description = this.description
+
+      this.cottage.id = sessionStorage.getItem("cottageId")
+
+      let body = JSON.stringify(this.cottage)
+  
+      this.http.put<any>(this.endpoint.COTTAGES, body, options).pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.error instanceof Error) {
+            alert("Bad request, please try again later.");
+          } else {
+            alert("Cottage has reservations, you can not change its data.")
+          }
+          return EMPTY;
+        })).subscribe()
+    }
+
+    deleteRoom(roomId: Number){
+      const headers = { 'content-type': 'application/json',
+                      'Authorization': 'Bearer ' + sessionStorage.getItem("token")}  
+      let options = { headers: headers };
+
+      this.http.delete<any>(this.endpoint.COTTAGES +  sessionStorage.getItem('cottageId') + '/delete-room/' + roomId,options).pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.error instanceof Error) {
+            alert("Bad request, please try again later.");
+          } else {
+            alert("Cottage is or has been reserved and could not be deleted.");
+          }
+          return EMPTY;
+        })).subscribe(() => alert("Room is deleted."))
+
+    }
+
+    addRoom(){
+      let room : Room = new Room()
+      room.name = this.roomName
+      room.bedTypes = this.roomBadTypes
+
+      const headers = { 'content-type': 'application/json',
+      'Authorization': 'Bearer ' + sessionStorage.getItem("token")}  
+      let options = { headers: headers };
+
+      
+      let body = JSON.stringify(room)
+
+      this.http.post<any>(this.endpoint.ROOMS, body, options).pipe(
+        catchError((error: HttpErrorResponse) => {
+        if (error.error instanceof Error) {
+        alert("Bad request, please try again later.");
+        } else {
+        alert("Cottage has reservations, you can not change its data.")
+        }
+       return EMPTY;
+      }),
+      map(returnedRoom =>{
+
+        room.id = returnedRoom["id"]
+
+      })).subscribe(() =>{
+        this.cottage.id = sessionStorage.getItem("cottageId")
+        this.cottage.rooms.push(room)
+
+        let body = JSON.stringify(this.cottage)
+
+        this.http.put<any>(this.endpoint.COTTAGES, body, options).pipe(
+          catchError((error: HttpErrorResponse) => {
+          if (error.error instanceof Error) {
+          alert("Bad request, please try again later.");
+          } else {
+          alert("Cottage has reservations, you can not change its data.")
+          }
+        return EMPTY;
+        })).subscribe()
+      })
+
+      
+          
+    }
+
+    selectFiles(event: any): void {
+      this.selectedFiles = event.target.files;
+    
+      this.previews = [];
+      if (this.selectedFiles && this.selectedFiles[0]) {
+        const numberOfFiles = this.selectedFiles.length;
+        for (let i = 0; i < numberOfFiles; i++) {
+          const reader = new FileReader();
+    
+          reader.onload = (e: any) => {
+            this.previews.push(e.target.result);
+          };
+    
+          reader.readAsDataURL(this.selectedFiles[i]);
+        }
+      }
+    }
+  
+    uploadFiles(): void {
+      if (this.selectedFiles) {
+        for (let i = 0; i < this.selectedFiles.length; i++) {
+          this.upload(this.selectedFiles[i]);
+        }
+      }
+    }
+  
+    upload(file: File): void {
+    
+      const formData: FormData = new FormData(); 
+      formData.append('file', file);
+  
+      const headers = { 'Authorization': 'Bearer ' + sessionStorage.getItem("token")}  
+      let options = { headers: headers };
+      
+      this.http.post<any>(this.endpoint.UPLOAD + 'add-cottage-picture/' + this.cottage.id, formData, options)
+              .subscribe()
+      
+        
+        }  
+  }
 
